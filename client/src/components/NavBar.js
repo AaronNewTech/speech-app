9283d65161178356bff1a2bea9fc3b6ddab277a1
Aaron <arnhsmith@gmail.com>
2023-09-22 00:21:49 -0400

    social media icons added to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,64 +8,77 @@ function NavBar() {
   return (
     <div>
       <nav className="navbarStyles">
-        <div>
-          <NavLink to="/"> Home </NavLink>
-        </div>
-        <div>
-          <NavLink to="/login"> Login </NavLink>
-        </div>
-        <div>
-          <NavLink to="/about"> About </NavLink>
-        </div>
+        <div className="navbar-links">
+          <div>
+            <NavLink to="/"> Home </NavLink>
+          </div>
+          <div>
+            <NavLink to="/login"> Login </NavLink>
+          </div>
+          <div>
+            <NavLink to="/about"> About </NavLink>
+          </div>
 
-        {/* <li>
-          <NavLink to="/videos"> Videos </NavLink>
-        </li>
-        <li>
-          <NavLink to="/rock-paper-scissors-game">Rock Paper Scissors</NavLink>
-        </li> */}
+          {user ? (
+            <div className="dropdown">
+              <a>My Cards</a>
+              <div>
+                <div className="dropdown-content">
+                  <div>
+                    <NavLink to="/create-card"> Create Cards </NavLink>
+                  </div>
+                  <div>
+                    <NavLink to="/favorite-cards"> Saved Cards </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <></>
+          )}
 
-        {user ? (
+          {/* Dropdown menu */}
           <div className="dropdown">
-            <a>My Cards</a>
+            <a>Speech Practice</a>
             <div>
               <div className="dropdown-content">
                 <div>
-                  <NavLink to="/create-card"> Create Cards </NavLink>
+                  <NavLink to="/videos"> Video Speech Trainer </NavLink>
                 </div>
                 <div>
-                  <NavLink to="/favorite-cards"> Saved Cards </NavLink>
+                  <NavLink to="/speech-practice"> First Words </NavLink>
                 </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <></>
-        )}
-
-        {/* Dropdown menu */}
-        <div className="dropdown">
-          <a>Speech Practice</a>
-          <div>
-            <div className="dropdown-content">
-              <div>
-                <NavLink to="/videos"> Video Speech Trainer </NavLink>
-              </div>
-              <div>
-                <NavLink to="/speech-practice"> First Words </NavLink>
-              </div>
-              <div>
-                <NavLink to="/rock-paper-scissors-game">
-                  Games
-                </NavLink>
+                <div>
+                  <NavLink to="/rock-paper-scissors-game">Games</NavLink>
+                </div>
               </div>
             </div>
           </div>
         </div>
-
-        {/* <li>
-          <NavLink to="/speech-practice"> Speech Practice </NavLink>
-        </li> */}
+        <div className="social-icons">
+          <NavLink to="https://www.facebook.com/">
+            <img
+              id="facebook"
+              src="/images/Social/facebook.png"
+              alt="facebook-icon"
+            />
+          </NavLink>
+          <NavLink to="https://instagram.com/">
+            <img
+              id="instagram"
+              src="/images/Social/instagram.png"
+              alt="instagram-icon"
+              
+            />
+          </NavLink>
+          <NavLink to="https://twitter.com">
+            <img
+              id="twitter"
+              src="/images/Social/twitter.png"
+              alt="twitter-icon"
+            />
+          </NavLink>
+        </div>
       </nav>
     </div>
   );
